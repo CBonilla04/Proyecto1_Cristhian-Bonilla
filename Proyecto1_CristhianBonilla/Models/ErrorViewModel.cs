bcff8d354ef73a4f30b3adec076861463a7ec30a
cbonilla <cristhian12bm@gmail.com>
2024-07-03 21:32:39 -0600

    initial commit

---
+++ Proyecto1_CristhianBonilla/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto1_CristhianBonilla.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
